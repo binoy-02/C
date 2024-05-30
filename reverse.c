72df273511a9cfb1b36ccb7d55097e48875ff902
binoy-02 <bnyjolly@gmail.com>
2024-05-30 23:01:14 +0530

    reverseOfNumber

---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int reverse(int num)
+{
+    int rev=0,rem;
+    while(num!=0)
+    {
+        rev=rev*10+(num%10);
+        num=num/10;
+    }
+    return rev;
+
+}
+int main()
+{   
+    int num,result;
+    scanf("%d",&num);
+    result=reverse(num);
+    printf("%d",result);
+    return 0;
+}
